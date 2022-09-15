dca5926312c60d2de0923d9e14e83c56d33ead93
Caio Vinicius Fernandes Antonio <caiocaio620@gmail.com>
2022-09-15 16:03:29 -0300

    Upload

--- src/caixeiroviajantealgoritmogenetico/CaixeiroViajanteAlgoritmoGenetico.java
+++ src/caixeiroviajantealgoritmogenetico/CaixeiroViajanteAlgoritmoGenetico.java
@@ -5,55 +5,22 @@ import java.util.ArrayList;
 public class CaixeiroViajanteAlgoritmoGenetico {
 	
 	public static void main(String[] args) {
-		FileManager fileManager = new FileManager();
-		ArrayList<String> text = fileManager.stringReader("./data/Teste.txt");
-
-		int nVertex = 0;
-		int graph[][] = null;
-		int i;
-
-		for (i = 0; i < text.size(); i++) {
-			String line = text.get(i);
-			if (i == 0) {
-				nVertex = Integer.parseInt(line.trim());
-				graph = new int[nVertex][nVertex];
-			} else {
-				int oriVertex = Integer.parseInt(line.split(" ")[0]);
-				String splits[] = line.substring(line.indexOf(" "), line.length()).split(";");
-				for (String part : splits) {
-					String edgeData[] = part.split("-");
-					int targetVertex = Integer.parseInt(edgeData[0].trim());
-					int weight = Integer.parseInt(edgeData[1]);
-
-					graph[oriVertex][targetVertex] = weight;
-					graph[targetVertex][oriVertex] = weight;
-				}
-
-			}
-
-		}
-
-		
-		  
-		  for (i = 0; i < nVertex; i++) { for (int j = 0; j < nVertex; j++) {
-		 System.out.print(" " + graph[i][j] + " "); } System.out.println(); }
-		  
-		  
-	}
-	
-	public int[] geraIndividuo(int tamanho, int numeroIndividuos){
-		int[] genes = new int[tamanho];
-		
-		for(int i = 0; i < tamanho;i++) {
 			
-		}
+		 int kIndividuos = 10000;
+		 ArrayList<Individuo> populacao = new ArrayList<Individuo>();
+		 for(int i = 0; i< kIndividuos; i++) {
+			 Individuo individuo = new Individuo(100); 
+			 individuo.criarIndividuo();
+			 individuo.imprimiGenes();
+			 populacao.add(individuo);
+			 
+		 }
+		 
+		// System.out.println(populacao);
 		
-		
-		return genes;
-		
+		 
+		  
 	}
-	
-	
 	 
 
 }
--- src/caixeiroviajantealgoritmogenetico/Individuo.java
+++ src/caixeiroviajantealgoritmogenetico/Individuo.java
@@ -1,5 +1,62 @@
 package caixeiroviajantealgoritmogenetico;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Individuo {
+	private int tamanho;
+	private int[] genes;
+	
+	public Individuo(int tamanho) {
+		this.tamanho = tamanho;
+		this.genes = new int[this.getTamanho()];
+		for(int i = 0; i < tamanho; i++) {
+			this.genes[i] = -1;
+		}
+	}
+	
+	public void criarIndividuo() {
+		Random gerador = new Random();
+		int posicao = 0;
+		for(int i = 0; i<this.getTamanho(); i++) {
+			
+			do {
+				
+				posicao = gerador.nextInt(this.getTamanho());
+				if(this.genes[posicao] == -1) {
+				 this.genes[posicao] = i;
+				 break;
+				}
+
+			}while(this.genes[posicao] != -1);
+		}
+	}
+	
+	
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public int[] getGenes() {
+		return genes;
+	}
+
+	public void setGenes(int[] genes) {
+		this.genes = genes;
+	}
+	
+	public void imprimiGenes(){
+		ArrayList<Integer> genes = new ArrayList<Integer>();
+		for(int i = 0; i<this.tamanho;i++) {
+			genes.add(this.genes[i]);
+		}
+		
+		System.out.println(genes);
+	}
+		
 
 }
